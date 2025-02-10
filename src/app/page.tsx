81c25edcb699a0d928c3e6d9af2030b56783f18d
Lucky <lucky@Luckys-MBP.fritz.box>
2025-02-10 20:13:11 +0100

    Add landing page content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
-import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <Image
+        {/* <Image
           className={styles.logo}
           src="/next.svg"
           alt="Next.js logo"
@@ -18,10 +17,11 @@ export default function Home() {
             Get started by editing <code>src/app/page.tsx</code>.
           </li>
           <li>Save and see your changes instantly.</li>
-        </ol>
+        </ol> */}
 
         <div className={styles.ctas}>
-          <a
+          <div className={styles.title}>fkinpay.com</div>
+          {/* <a
             className={styles.primary}
             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
             target="_blank"
@@ -43,11 +43,14 @@ export default function Home() {
             className={styles.secondary}
           >
             Read our docs
-          </a>
+          </a> */}
         </div>
       </main>
       <footer className={styles.footer}>
-        <a
+        <div className={styles.footerText}>
+          Ha llegado el momento de digitalizar los restaurantes.
+        </div>
+        {/* <a
           href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
           target="_blank"
           rel="noopener noreferrer"
@@ -88,7 +91,7 @@ export default function Home() {
             height={16}
           />
           Go to nextjs.org →
-        </a>
+        </a> */}
       </footer>
     </div>
   );
